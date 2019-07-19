773a5beb5c868ab9358c47dcc3b051d7c8e62201
cmuth001 <cmuth001@odu.edu>
2019-07-19 01:58:14 -0400

    mongoose db connected

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var  Controller = require('./controller/controller.js');
 var  express  = require("express");
 var app = express();
+let mongoose = require('mongoose');
+var db = mongoose.connection;
 
+// Added check for DB connection
+if(!db)
+    console.log("Error connecting db");
+else
+    console.log("Db connected successfully");
 // creating an object
 var contObj = new Controller();
 
--- model/services.js
+++ model/services.js
@@ -1,10 +1,16 @@
+
 var data = { 1: "Tony",
              2: "Lisa",
              3: "Michael",
              4:"Ginger"
             };
-class Services{
+            
 
+
+class Services{
+    get_mongo_connection(){
+
+    }
     get_users(){
         return(data);
     }
